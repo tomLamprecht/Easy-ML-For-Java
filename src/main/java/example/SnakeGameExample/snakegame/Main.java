3c1d783997cd0f08ab440c25547093dc7800302a
tlampre <tom.lamprecht@ssi-schaefer.com>
2022-12-07 15:47:42 +0100

    adjusted AI values for Snake Example

--- src/main/java/example/SnakeGameExample/snakegame/Main.java
+++ src/main/java/example/SnakeGameExample/snakegame/Main.java
@@ -18,7 +18,7 @@ import de.fhws.ai.neuralnetwork.NeuralNet;
 
 public class Main {
 
-    public static final int MAX_GENS = 100;
+    public static final int MAX_GENS = 500;
     public static final Selector<NeuralNetIndividual> SELECTOR = new RouletteWheelSelector<>(0.4, true);
     public static final Recombiner<NeuralNetIndividual> RECOMBINER = new NNUniformCrossoverRecombiner(2);
     public static final Mutator<NeuralNetIndividual> MUTATOR = new NNRandomMutator(0.5, 0.6, new Randomizer(-0.1,0.1), 0.01);
@@ -27,11 +27,11 @@ public class Main {
         NeuralNetSupplier nn =() -> new NeuralNet.Builder(25, 4)
                 .build();
 
-        NeuralNetFitnessFunction fitnessFunction = (neural) -> new SnakeAi(neural).startPlaying(1000);
+        NeuralNetFitnessFunction fitnessFunction = (neural) -> new SnakeAi(neural).startPlaying(100);
 
         PopulationSupplier<NeuralNetIndividual> populationSupplier = new NeuralNetPopulationSupplier(nn, fitnessFunction, 1000);
 
-        GeneticAlgorithm<NeuralNetIndividual> ga = new GeneticAlgorithm.Builder<NeuralNetIndividual>(populationSupplier,MAX_GENS, SELECTOR)
+        GeneticAlgorithm<NeuralNetIndividual> ga = new GeneticAlgorithm.Builder<>(populationSupplier, MAX_GENS, SELECTOR)
                 .withRecombiner(RECOMBINER)
                 .withMutator(MUTATOR)
                 .withLoggers(new ConsoleLogger(), new GraphPlotLogger(-1, "plot",
