cd206ff2f4f65d505fc5a23171858a47b25a8e73
tomLamprecht <79751041+tomLamprecht@users.noreply.github.com>
2022-12-19 23:29:18 +0100

    added ThrowingFunction

---
+++ src/main/java/de/fhws/easyml/utility/throwingintefaces/ThrowingFunction.java
@@ -0,0 +1,18 @@
+package de.fhws.easyml.utility.throwingintefaces;
+
+import java.util.function.Function;
+
+public interface ThrowingFunction<T, R, E extends Exception> {
+    R apply( T t ) throws E;
+
+    static <T, R, E extends Exception> Function<T, R> unchecked( ThrowingFunction<T, R, E> throwingFunction ) {
+        return t -> {
+            try {
+               return throwingFunction.apply( t );
+            } catch ( Exception e ) {
+                throw new RuntimeException( e );
+            }
+        };
+    }
+
+}
